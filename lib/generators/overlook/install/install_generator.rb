fe35cd6b59ca8c2455d47b202f00467582cc5bf8
Jim Neath <jimneath@googlemail.com>
2012-03-25 13:47:10 +0100

    Added route namespace

--- lib/generators/overlook/install/install_generator.rb
+++ lib/generators/overlook/install/install_generator.rb
@@ -1,6 +1,12 @@
 module Overlook
   class InstallGenerator < Rails::Generators::Base
     source_root File.expand_path('../templates', __FILE__)
+    
+    def add_namespace
+      inject_into_file 'config/routes.rb', "\n  namespace :admin do\n  end\n", {
+        after: /routes.draw do$/
+      }
+    end
   
     def install_files
       source = self.class.source_root
--- lib/overlook/version.rb
+++ lib/overlook/version.rb
@@ -1,3 +1,3 @@
 module Overlook
-  VERSION = "0.0.3"
+  VERSION = "0.0.4"
 end
